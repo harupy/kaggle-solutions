c7ce367b54aee2b9609d2fdb1474b81eab87ff44
harupy <hkawamura0130@gmail.com>
2020-01-25 23:30:39 +0900

    Fix urls

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Header as SemHeader, Menu } from 'semantic-ui-react';
 import StarCounter from './StarCounter';
-import { KAGGLE_BLUE, KAGGLE_GRAY } from './constants';
+import { REPO_NAME, REPO_URL, KAGGLE_BLUE, KAGGLE_GRAY } from './constants';
 
 const Header: React.SFC = () => (
   <Menu borderless inverted fixed="top" size="massive" style={{ backgroundColor: KAGGLE_GRAY }}>
     {/* Render as an anchor tag to enable highlighting on hover. */}
     <Menu.Item as="a">
       <SemHeader>
-        <a href="/" style={{ color: KAGGLE_BLUE, fontWeight: 'bold' }}>
+        <a href={`/${REPO_NAME}`} style={{ color: KAGGLE_BLUE, fontWeight: 'bold' }}>
           Kaggle Solutions
         </a>
       </SemHeader>
     </Menu.Item>
     <Menu.Item position="right" as="a">
-      <StarCounter url="https://github.com/harupy/kaggle-solutions" />
+      <StarCounter url={REPO_URL} />
     </Menu.Item>
   </Menu>
 );
