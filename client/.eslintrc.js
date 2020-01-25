9e962afcf6bcf558122464286ca37e84f9542b5f
harupy <hkawamura0130@gmail.com>
2020-01-25 17:59:56 +0900

    Fix eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -4,14 +4,16 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
-    'prettier/@typescript-eslint'
+    // 'prettier/@typescript-eslint'
   ],
   plugins: ['@typescript-eslint'],
   env: { node: true, es6: true },
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaVersion: 2018,
     sourceType: 'module',
-    project: './tsconfig.json'
+    project: './tsconfig.json',
   },
-  rules: {}
+  ignorePatterns: ['src/serviceWorker.ts', 'node_modules/'],
+  rules: {},
 };
