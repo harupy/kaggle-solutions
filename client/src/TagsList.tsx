c246700280543ff9cf73d45a4cb5ff4aa5d7623e
harupy <hkawamura0130@gmail.com>
2020-01-25 23:58:05 +0900

    Use category

--- client/src/TagsList.tsx
+++ client/src/TagsList.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Label } from 'semantic-ui-react';
+import { Category } from './CompetitionsTable';
 
 interface TagsListProps {
-  tags: (string | undefined)[];
+  categories: Category[];
 }
 
-const TagsList: React.SFC<TagsListProps> = ({ tags }) => {
-  return <Label.Group>{tags.map(tag => tag && <Label key={tag}>{tag}</Label>)}</Label.Group>;
+const TagsList: React.SFC<TagsListProps> = ({ categories }) => {
+  return (
+    <Label.Group>
+      {categories.map(({ name }) => (
+        <Label key={name}>{name}</Label>
+      ))}
+    </Label.Group>
+  );
 };
 
 export default TagsList;
