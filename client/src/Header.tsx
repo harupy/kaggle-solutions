639e76003502485747f77889631ff4dde2404372
harupy <hkawamura0130@gmail.com>
2020-01-25 20:12:46 +0900

    Add link to title

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -5,8 +5,12 @@ import { KAGGLE_BLUE, KAGGLE_GRAY } from './constants';
 
 const Header: React.SFC = () => (
   <Menu borderless inverted fixed="top" size="massive" style={{ backgroundColor: KAGGLE_GRAY }}>
-    <Menu.Item>
-      <SemHeader style={{ color: KAGGLE_BLUE, fontWeight: 'bold' }}>Kaggle Solutions</SemHeader>
+    <Menu.Item as="a">
+      <SemHeader>
+        <a href="/" style={{ color: KAGGLE_BLUE, fontWeight: 'bold' }}>
+          Kaggle Solutions
+        </a>
+      </SemHeader>
     </Menu.Item>
     {/* Render as an anchor tag to enable highlighting on hover. */}
     <Menu.Item position="right" as="a">
