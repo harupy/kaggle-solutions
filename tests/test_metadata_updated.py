03ab3ded8612d4b40628280ec87dc0617193e57b
harupy <hkawamura0130@gmail.com>
2020-01-26 23:54:44 +0900

    Fix test

--- tests/test_metadata_updated.py
+++ tests/test_metadata_updated.py
@@ -7,4 +7,7 @@ def test_metadata_updated():
     script_path = 'tools/merge_metadata.py'
     msg = (f'The content of `{metadata_path}` does not equal to '
            f'the result of `{script_path}`. Please run `{script_path}`.')
-    assert merge_metadata() == read_json(metadata_path), msg
+    actual = read_json(metadata_path)
+    actual.sort(key=lambda m: m['enabledDate'], reverse=True)
+    expected = merge_metadata()
+    assert actual == expected, msg
