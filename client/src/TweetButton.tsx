7f4c64ed73c4f631ae6bc8af1caee0638aa5dd2f
harupy <hkawamura0130@gmail.com>
2020-01-26 17:41:11 +0900

    Fix tweet default text

--- client/src/TweetButton.tsx
+++ client/src/TweetButton.tsx
@@ -6,8 +6,10 @@ interface TweetButtonProps {
 }
 
 const TweetButton: React.SFC<TweetButtonProps> = ({ url }) => {
+  // Insert a carriage return to make it easier for users to add comments.
+  const encoded = encodeURI(`\r\n${url}`);
   return (
-    <a href={`https://twitter.com/intent/tweet?text=${url}`}>
+    <a href={`https://twitter.com/intent/tweet?text=${encoded}`}>
       <Icon name="twitter" size="large" />
     </a>
   );
