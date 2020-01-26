a6f09efbd34703d2e7acd91a7bf57434fa5c614f
harupy <hkawamura0130@gmail.com>
2020-01-27 00:03:31 +0900

    Fix test

--- tests/test_metadata_updated.py
+++ tests/test_metadata_updated.py
@@ -1,3 +1,5 @@
+import unittest
+
 from tools.merge_metadata import merge_metadata
 from tools.utils import read_json
 
@@ -8,6 +10,8 @@ def test_metadata_updated():
     msg = (f'The content of `{metadata_path}` does not equal to '
            f'the result of `{script_path}`. Please run `{script_path}`.')
     actual = read_json(metadata_path)
-    actual.sort(key=lambda m: m['enabledDate'], reverse=True)
+    # actual.sort(key=lambda m: m['enabledDate'], reverse=True)
     expected = merge_metadata()
-    assert actual == expected, msg
+    # expected.sort(key=lambda m: m['enabledDate'], reverse=True)
+    tc = unittest.TestCase('__init__')
+    tc.assertCountEqual(actual, expected, msg)
