d346a83eb2cad67e4243d69d80bf62e47d66446d
harupy <hkawamura0130@gmail.com>
2020-02-06 01:56:32 +0900

    Remove trailing whitespaces

--- dev/lint.sh
+++ dev/lint.sh
@@ -8,7 +8,7 @@ lines=$(git ls-tree -r HEAD --name-only | xargs egrep -nI " +$" | cut -f 1,2 -d 
 if [ ! -z "$lines" ]; then
   printf "%s\n" "${lines[@]}"
   printf "\n%s\n" "Found trailing whitespaces."
-  exit 1 
-fi 
+  exit 1
+fi
 
 pipenv run flake8 tools tests
