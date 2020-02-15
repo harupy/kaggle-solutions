ce574462dd37701233a8460ac0d78a391b6b8553
harupy <hkawamura0130@gmail.com>
2020-02-15 18:49:33 +0900

    Add comments

--- dev/lint.sh
+++ dev/lint.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
 
+# This script detects the following violations for all tracked files:
+# - trailing whitespaces
+# - no new line at EOF
+
 ws_lines=""  # lines containing trailing whitespaces.
 nl_files=""  # files that don't end with a newline.
 
+# Iterate over all tracked files.
 for file in $(git diff --name-only origin/master | sed -e 's/^/.\//')
 do
+  # trailing whitespaces
   lines=$(egrep -rnIH " +$" $file | cut -f-2 -d ":")
   if [ ! -z "$lines" ]; then
     ws_lines+=$([[ -z "$ws_lines" ]] && echo "$lines" || echo $'\n'"$lines")
   fi
 
+  # no new line at EOF
   if [ ! -z "$(tail -c 1 $file)" ]; then
     nl_files+=$([[ -z "$nl_files" ]] && echo "$file" || echo $'\n'"$file")
   fi
@@ -17,6 +24,7 @@ done
 
 exit_code=0
 
+# If violations detected, change the exit code to 1 to make the CI fail.
 if [ ! -z "$ws_lines" ]; then
   printf "\n%s\n" "# Trailing whitespace"
   printf "%s\n" "${ws_lines[@]}"
